50d6cb6c7e20b181d097907bd119e5f968a3d933
jamabaiz <contact@janbartula.pl>
2022-03-17 23:19:44 +0100

    Lab 2

---
+++ lab2/fork.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "pomiar_czasu.h"
+#include <sys/wait.h>
+
+int zmienna_globalna = 0;
+
+int main() {
+
+    int pid, wynik, i;
+    inicjuj_czas();
+    zmienna_globalna = 0;
+
+    for (i = 0; i < 1000; i++) {
+        pid = fork();
+        if (pid == 0) {
+            zmienna_globalna++;
+            wynik = execv("./program", NULL);
+            if (wynik == -1)
+                printf("Proces potomny nie wykonal programu\n");
+
+            exit(0);
+        } else {
+            wait(NULL);
+        }
+    }
+    printf("Zmienna Globalna: %d\n", zmienna_globalna);
+    drukuj_czas();
+}
+
+  
