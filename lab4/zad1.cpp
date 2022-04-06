882f5eb2e7af7f9c3017adeabb93a26a031435b9
jamabaiz <contact@janbartula.pl>
2022-04-06 19:21:20 +0200

    Lab 4 init

---
+++ lab4/zad1.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+void *increment(void *arg) {
+
+    return 0;
+}
+
+int main() {
+
+    printf("Hello world!\n");
+
+    return 0;
+}
